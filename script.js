a676b1fabce4880abc78890b7d1f851ff1049d70
Жангир Уакасов <zangiruakasov@gmail.com>
2022-10-26 20:18:39 +0600

    new version

--- script.js
+++ script.js
@@ -18,12 +18,27 @@
     // console.log(arguments)
 
 
-function avg(){
-    let sum = 0;
-    for(let i = 0; i < arguments.length; i++){
-        sum += arguments[i];
-    }
-    return sum / arguments.length;
-}
-console.log(avg(1,2,3,45,45,54,4,5,5,8,5,8))
+// function avg(){
+//     let sum = 0;
+//     for(let i = 0; i < arguments.length; i++){
+//         sum += arguments[i];
+//     }
+//     return sum / arguments.length;
+// }
+// console.log(avg(1,2,3,45,45,54,4,5,5,8,5,8))
     // console.log(arguments)
+
+
+    // function repeat(str, n){      думал что так, потом узнал о функции repaat.
+    //     return str * n
+    // }
+
+    // console.log(repeat("asd", 2))
+
+
+
+    function repeat(str, n = 2) {
+        return str.repeat(n);
+    }
+    
+    console.log(repeat('asd', 2));
